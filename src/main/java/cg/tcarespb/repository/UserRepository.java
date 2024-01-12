4e6bef7ff82b3a0d49b784864abc7e6ad364e690
QuocHuy248 <quochuy248@gmail.com>
2024-01-12 14:41:01 +0700

    update reset password, admin service

--- src/main/java/cg/tcarespb/repository/UserRepository.java
+++ src/main/java/cg/tcarespb/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package cg.tcarespb.repository;
 
 import cg.tcarespb.models.User;
+import cg.tcarespb.service.admin.response.AdminEmployeeResponse;
+import cg.tcarespb.service.admin.response.AdminUserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,String> {
+    @Query("SELECT NEW  cg.tcarespb.service.admin.response.AdminUserResponse(u.id,u.personID,u.firstName,u.lastName,u.gender)  FROM User u")
+    Page<AdminUserResponse> getAllUser(Pageable pageable);
 }
