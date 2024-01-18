9d0be8d86d70b98513aba429aa507db52bb981c2
ANHTUANPHUNG <132957644+ANHTUANPHUNG@users.noreply.github.com>
2024-01-18 11:23:01 +0700

    tuandev

--- src/main/java/cg/tcarespb/service/employee/response/EmployeeFilterResponse.java
+++ src/main/java/cg/tcarespb/service/employee/response/EmployeeFilterResponse.java
@@ -30,6 +30,7 @@ public class  EmployeeFilterResponse {
     private List<String> skillName;
     private List<String> serviceName;
     private List<String> infoName;
+    private String cartId;
 
     public EmployeeFilterResponse(String id,String nameLocation, String firstName, String lastName,String bio, String descriptionAboutMySelf, EExperience experience, Double longitude, Double latitude, String address) {
         this.id = id;
